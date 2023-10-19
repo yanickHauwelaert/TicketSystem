5104f7cd78fee90d2f315bdf54d7056a9f474321
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-10-19 15:24:02 +0200

    Add AccountRepository

---
+++ TicketSystem.Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketSystem.Core.Entities;
+using TicketSystem.Core.Repositories.Interfaces;
+using TicketSystem.Infrastructure.Data;
+
+namespace TicketSystem.Infrastructure.Repositories;
+
+public class AccountRepository: IAccountRepository
+{
+    protected readonly ApplicationDbContext _dbContext;
+
+    public AccountRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<ICollection<ApplicationUser>> GetAll()
+    {
+        var users = await _dbContext.Users.ToListAsync();
+        return users;
+    }
+
+    public async Task<ApplicationUser> GetByIdAsync(Guid id)
+    {
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+        return user;
+    }
+
+    public async Task<ApplicationUser> AddAsync(ApplicationUser user)
+    {
+        _dbContext.Users.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<ApplicationUser> UpdateAsync(ApplicationUser user)
+    {
+        _dbContext.Users.Update(user);
+        await _dbContext.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<ApplicationUser> DeleteAysnc(ApplicationUser user)
+    {
+        _dbContext.Users.Remove(user);
+        await _dbContext.SaveChangesAsync();
+        return user;
+    }
+}
