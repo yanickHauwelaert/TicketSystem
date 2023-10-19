442f99265326f736a2282570beefd151cf4e6a5a
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-10-19 15:18:17 +0200

    Add CategoryRepository

---
+++ TicketSystem.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using TicketSystem.Core.Entities;
+using TicketSystem.Core.Repositories.Interfaces;
+using TicketSystem.Infrastructure.Data;
+
+namespace TicketSystem.Infrastructure.Repositories;
+
+public class CategoryRepository: BaseRepository<Category>, ICategoryRepository
+{
+    public CategoryRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {
+    }
+}
