0bc039282756a4b9a4235167c903f5c414a6209e
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-10-19 15:17:14 +0200

    Add TicketRepository

---
+++ TicketSystem.Infrastructure/Repositories/TicketRepository.cs
@@ -0,0 +1,12 @@
+﻿using TicketSystem.Core.Entities;
+using TicketSystem.Core.Repositories.Interfaces;
+using TicketSystem.Infrastructure.Data;
+
+namespace TicketSystem.Infrastructure.Repositories;
+
+public class TicketRepository: BaseRepository<Ticket>,ITicketRepository
+{
+    public TicketRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {
+    }
+}
