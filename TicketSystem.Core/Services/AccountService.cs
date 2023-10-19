25a73873c33151346b8df5eddf384f3a0cc82850
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-10-19 20:32:04 +0200

    Add account service

---
+++ TicketSystem.Core/Services/AccountService.cs
@@ -0,0 +1,46 @@
+﻿using TicketSystem.Core.Entities;
+using TicketSystem.Core.Repositories.Interfaces;
+using TicketSystem.Core.Services.Interfaces;
+
+namespace TicketSystem.Core.Services;
+
+public class AccountService: IAccountService
+{
+    protected readonly IAccountRepository _accountRepository;
+
+    public AccountService(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository;
+    }
+
+
+    public Task<ICollection<ApplicationUser>> GetAllUsers()
+    {
+        var users = _accountRepository.GetAll();
+        return users;
+    }
+
+    public Task<ApplicationUser> GetUserById(Guid id)
+    {
+        var user = _accountRepository.GetByIdAsync(id);
+        return user;
+    }
+
+    public Task<ApplicationUser> AddUser(ApplicationUser user)
+    {
+        var addedUser = _accountRepository.AddAsync(user);
+        return addedUser;
+    }
+
+    public Task<ApplicationUser> UpdateUser(ApplicationUser user)
+    {
+        var updatedUser = _accountRepository.UpdateAsync(user);
+        return updatedUser;
+    }
+
+    public Task<ApplicationUser> DeleteUser(ApplicationUser user)
+    {
+        var deletedUser = _accountRepository.DeleteAysnc(user);
+        return deletedUser;
+    }
+}
