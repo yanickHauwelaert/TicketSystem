36b100ba652eb803ea185a9536bf8a89dd0e08ea
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-10-19 15:04:28 +0200

    Add DbContext

---
+++ TicketSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TicketSystem.Core.Entities;
+
+namespace TicketSystem.Infrastructure.Data;
+
+public class ApplicationDbContext: IdentityDbContext<ApplicationUser,IdentityRole<Guid>,Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+}
