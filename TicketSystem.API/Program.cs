e5db779fa87b29dc2b2a7f1ab3a6d55c1579c723
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-11-07 13:46:23 +0100

    Configerd swagger

--- TicketSystem.API/Program.cs
+++ TicketSystem.API/Program.cs
@@ -1,5 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using TicketSystem.Core.Entities;
 using TicketSystem.Core.Repositories.Interfaces;
 using TicketSystem.Core.Services;
@@ -34,10 +38,90 @@ builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ITicketService, TicketService>();
 
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",new OpenApiInfo
+    {
+        Title = "Ticket system",
+        Version = "v1"
+    });
+    
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwtOptions =>
+{
+    var config = builder.Configuration;
+    jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidIssuer = config["JWTConfiguration:Issuer"],
+        ValidAudience = config["JWTConfiguration:Audience"],
+        IssuerSigningKey =
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWTConfiguration:SigningKey"]))
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("CanRead", policy =>
+    {
+        policy.RequireAssertion(context =>
+        {
+            if (context.User.IsInRole("Admin") || context.User.IsInRole("User"))
+                return true;
+            return false;
+        });
+    });
+    options.AddPolicy("CanEdit", policy =>
+    {
+        policy.RequireAssertion(context =>
+        {
+            if (context.User.IsInRole("Admin") || context.User.IsInRole("User"))
+                return true;
+            return false;
+        });
+    });
+    options.AddPolicy("CanUpdate", policy =>
+    {
+        policy.RequireAssertion(context =>
+        {
+            if (context.User.IsInRole("Admin") || context.User.IsInRole("User"))
+                return true;
+            return false;
+        });
+    });
+    options.AddPolicy("CanDelete", policy =>
+    {
+        policy.RequireAssertion(context =>
+        {
+            if (context.User.IsInRole("Admin") || context.User.IsInRole("User"))
+                return true;
+            return false;
+        });
+    });
+});
 
 var app = builder.Build();
 
