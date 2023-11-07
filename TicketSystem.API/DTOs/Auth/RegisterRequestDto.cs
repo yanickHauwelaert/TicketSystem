4fb20b5e533fb413e9da05fb2871f75f23d3c27b
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-11-07 16:15:34 +0100

    Add RegisterRequestDto

---
+++ TicketSystem.API/DTOs/Auth/RegisterRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketSystem.API.DTOs.Auth;
+
+public class RegisterRequestDto
+{
+    [Required(ErrorMessage = "Please enter your first name")]
+    public string Firstname { get; set; }
+
+    [Required(ErrorMessage = "Please enter your last name")]
+    public string Lastname { get; set; }
+
+    [Required(ErrorMessage = "Please enter your email")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Please enter your password")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Please enter a username")]
+    public string Username { get; set; }
+}
