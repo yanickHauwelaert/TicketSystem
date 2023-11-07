a2b0e77d20b7243a3a2b5c3571a6f7fb49c0fb15
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-11-07 16:19:35 +0100

    Add authcontroller

---
+++ TicketSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using TicketSystem.API.DTOs.Auth;
+using TicketSystem.Core.Entities;
+
+namespace TicketSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        protected readonly UserManager<ApplicationUser> _userManager;
+        protected readonly SignInManager<ApplicationUser> _signInManager;
+        protected readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto login)
+        {
+            var user = await _userManager.FindByEmailAsync(login.Username);
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, login.Password, false, false);
+
+            if (!result.Succeeded)
+                return Unauthorized();
+
+            JwtSecurityToken token = await GenerateTokenAsync(user);
+            string serializedToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new LoginResponseDto
+            {
+                Token = serializedToken
+            });
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto register)
+        {
+            var passwordHasher = new PasswordHasher<ApplicationUser>();
+            var user = new ApplicationUser
+            {
+                Id = new Guid(),
+                UserName = register.Username,
+                Email = register.Email,
+                FirstName = register.Firstname,
+                LastName = register.Lastname
+            };
+
+            user.PasswordHash = passwordHasher.HashPassword(user, register.Password);
+
+            var result = await _userManager.CreateAsync(user, register.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            await _userManager.AddToRoleAsync(user, "User");
+
+            return Ok();
+        }
+
+        private async Task<JwtSecurityToken> GenerateTokenAsync(ApplicationUser user)
+        {
+            var claims = new List<Claim>();
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            claims.AddRange(userClaims);
+
+            var roleClaims = await _userManager.GetRolesAsync(user);
+            foreach (var roleClaim in roleClaims)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, roleClaim));
+            }
+            var expirationDays = _configuration.GetValue<int>("JWTConfiguration:TokenExpirationDays");
+            var siginingKey = Encoding.UTF8.GetBytes(_configuration.GetValue<string>("JWTConfiguration:SigningKey"));
+            var token = new JwtSecurityToken
+            (
+                issuer: _configuration.GetValue<string>("JWTConfiguration:Issuer"),
+                audience: _configuration.GetValue<string>("JWTConfiguration:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromDays(expirationDays)),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(siginingKey),
+                    SecurityAlgorithms.HmacSha256)
+            );
+            return token;
+        }
+    }
+}
