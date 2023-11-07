ca9e668f51671c894a74091a85e8acf01c7ea42c
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-11-07 13:32:10 +0100

    Configer db connection

--- TicketSystem.API/Program.cs
+++ TicketSystem.API/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TicketSystem.Core.Entities;
+using TicketSystem.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("LocalDatabase");
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
