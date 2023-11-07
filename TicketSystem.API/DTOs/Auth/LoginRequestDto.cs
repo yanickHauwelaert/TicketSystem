63f6bfbc9c0c2b009096025b156526b197070d02
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-11-07 16:03:15 +0100

    Add Login Dtos

---
+++ TicketSystem.API/DTOs/Auth/LoginRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TicketSystem.API.DTOs.Auth;
+
+public class LoginRequestDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ TicketSystem.API/DTOs/Auth/LoginResponseDto.cs
@@ -0,0 +1,6 @@
+﻿namespace TicketSystem.API.DTOs.Auth;
+
+public class LoginResponseDto
+{
+    public string Token { get; set; }
+}
