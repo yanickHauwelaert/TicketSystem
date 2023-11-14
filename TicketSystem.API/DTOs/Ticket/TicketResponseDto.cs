5e9aae4a98d45e4d5e40526335b6f5a5a88456d9
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-11-14 19:21:47 +0100

    Add TicketResponseDto

---
+++ TicketSystem.API/DTOs/Ticket/TicketResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TicketSystem.API.DTOs.Ticket;
+
+public class TicketResponseDto
+{
+    public Guid Id { get; set; }
+    public string Subject { get; set; }
+    public string Description { get; set; }
+    public string Status  { get; set; }
+    public DateTime CreationDate { get; set; }
+    public Guid UserId { get; set; }
+    public Guid CategoryId { get; set; }
+}
